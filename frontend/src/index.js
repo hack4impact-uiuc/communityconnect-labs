46450bdbeaf50c288d1abd48cf187f6873178e42
kelley <kelleyc2@illinois.edu>
2019-10-06 14:27:15 -0500

    Putting in sample map

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import App from "./components/App";
+import App from "./components/app";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
