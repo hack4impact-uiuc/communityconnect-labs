90be9d9d6156c30ef81789b21c58ff4842703501
kelley <kelleyc2@illinois.edu>
2019-10-15 21:10:54 -0500

    Mongoengine backend (I hope)

---
+++ backend/api/models/Person.py
@@ -0,0 +1,17 @@
+from api.core import Mixin
+from .base import db
+from api.models import Email
+from flask_mongoengine import Document
+from mongoengine import *
+
+class Person(Document, Mixin):
+    """Person Collection."""
+
+    name = StringField(required=True)
+    emails = ListField(EmbeddedDocumentField(Email))
+
+    # def __init__(self, name: str):
+    #     self.name = name
+
+    def __repr__(self):
+        return f"<Person {self.name}>"
