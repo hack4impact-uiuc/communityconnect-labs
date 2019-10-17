045447a620f6b5c0316613e86ec9459e27c7ee51
Brandon Wang <sourbrandon@gmail.com>
2019-10-17 16:41:26 -0700

    update script to upload census response to databasea

--- populate_db.py
+++ populate_db.py
@@ -1,7 +1,9 @@
-import urllib.request
+import requests
 
 link = 'https://www2.census.gov/programs-surveys/decennial/2010/data/participation-rates-states/participationrates2010.txt'
 DELIM = " ||"
+
+DB_LINK = 'http://0.0.0.0:5000/census_response'
 
 class CensusResponse:
     def __init__(self, id, name, t, response2000, response2010):
@@ -14,9 +16,17 @@ class CensusResponse:
     def __str__(self):
         return f"ID: {self.id}\tName: {self.name}\tType: {self.type}\tResponse rate 2000: {self.response2000}\tResponse rate 2010: {self.response2010}"
 
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "rate2000": self.response2000,
+            "rate2010": self.response2010
+        }
+
 def parse_census_data(link):
-    request = urllib.request.Request(link)
-    result = urllib.request.urlopen(request).read().decode()
+    result = requests.get(link).text
     rows = result.strip().split("\n")
     responses = []
 
@@ -29,5 +39,6 @@ def parse_census_data(link):
 
 if __name__ == '__main__':
     responses = parse_census_data(link)
-    for r in responses:
-        print(r)
+
+    result = requests.post(DB_LINK, json=[r.json() for r in responses[]])
+    print(result.text)
