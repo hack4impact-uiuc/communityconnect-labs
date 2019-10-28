444b43b69f83d3617aeba7fc345b7d789aa2ee37
Alan Ren <alanren2@illinois.edu>
2019-10-27 19:21:56 -0500

    donezo

--- backend/api/models/CensusResponse.py
+++ backend/api/models/CensusResponse.py
@@ -10,12 +10,6 @@ class CensusResponse(Document, Mixin):
     county = StringField(required=True)
     rates = DictField(required=True)
 
-    # def __init__(self, id: str, name: str, type: str, rate2000: int, rate2010: int):
-    #     self.id = id
-    #     self.name = name
-    #     self.type = type
-    #     self.rate2000 = rate2000
-    #     self.rate2010 = rate2010
 
     def update(self, other):
         for i in self.rates:
