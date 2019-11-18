97c1b86a2001decc5b64aa0ffb11c7924f9ef82f
Danielle <31215735+dyang5200@users.noreply.github.com>
2019-11-17 19:49:08 -0600

    Created get_response_rate_by_period endpoint (#27)
    * Set map center and bounds
    
    * Added responses by period endpoint
    
    * Changed response_rates to rate
    
    * Changed Year to 4 digits
    
    * Changed get_response_rates endpoint
    
    * Changed request string in api wrapper
    
    * Removed persons
    
    * Delete hello world
    
    * Honoring brandon
    
    * Fixed documentation
    
    * Formatting

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -9,28 +9,48 @@ from .response_rates import *
 main = Blueprint("main", __name__)  # initialize blueprint
 
 """
-function that is called when you visit /response_rates
+function that is called when you visit /rate
 Parameters
-    date: date string with the format MMDDYYYY
-    year: year string with format YY
+    year: year string with format YYYY
     optional tract_id: 11-digit tract id string
     optional state: two digit id string
-Either date or year is required.
 """
 
 
-@main.route("/response_rates", methods=["GET"])
+@main.route("/rate", methods=["GET"])
 def get_response_rates():
     responses_rate = None
 
-    tract_id = request.args.get("tract_id", None)
-    date = request.args.get("date", None)
     year = request.args.get("year", None)
+    tract_id = request.args.get("tract_id", None)
     state = request.args.get("state", None)
 
-    if date:
-        response_rates = get_response_rates_by_date(date, tract_id, state)
-    elif year:
+    if year:
+        response_rates = get_last_response_rates_by_year(year, tract_id, state)
+    else:
+        return create_response(status=422, message="Missing request parameters")
+
+    return create_response(data={"response_rates": response_rates})
+
+
+"""
+function that is called when you visit /rates_per_period
+Parameters
+    year: year string with format YYYY
+    optional tract_id: 11-digit tract id string
+    optional state: two digit id string
+"""
+
+
+@main.route("/rates_per_period", methods=["GET"])
+def get_response_rates_per_period():
+    response_rate = None
+
+    year = request.args.get("year", None)
+    tract_id = request.args.get("tract_id", None)
+    state = request.args.get("state", None)
+
+    if year:
         response_rates = get_response_rates_by_year(year, tract_id, state)
     else:
         return create_response(status=422, message="Missing request parameters")
