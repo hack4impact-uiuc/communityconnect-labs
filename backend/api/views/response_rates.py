ff291f42b093f091d951c02373da6ae9a83921da
Danielle Yang <dy6@illinois.edu>
2019-11-17 19:14:43 -0600

    Changed get_response_rates endpoint

--- backend/api/views/response_rates.py
+++ backend/api/views/response_rates.py
@@ -5,8 +5,10 @@ import time
 
 DATE_FORMAT = "%m%d%Y"
 
+
 def get_year(date):
     return str(time.strptime(date, DATE_FORMAT).tm_year)
+
 
 '''
 returns a collection of CensusResponses
@@ -14,7 +16,9 @@ filtered by tract_id if tract_id parameter is not None
 '''
 def get_census_responses(tract_id, state):
     if state:
-        regex = re.compile("^{}.*".format(state)) # this regex searched for all tract_ids that begin with the state ID
+        regex = re.compile(
+            "^{}.*".format(state)
+        )  # this regex searched for all tract_ids that begin with the state ID
         return CensusResponse.objects(tract_id=regex)
     elif tract_id:
         return CensusResponse.objects(tract_id=tract_id)
@@ -22,34 +26,9 @@ def get_census_responses(tract_id, state):
         return CensusResponse.objects()
 
 '''
-Returns response rate by date
-Parameters:
-    date: string with format MMDDYYYY
-    optional tract_id: string 11 digit tract id
-    optional state id: string 2 digit state id
-Output:
-    [
-    {"tract_id": string, "rate": float},
-    ...
-    ]
-'''
-def get_response_rates_by_date(date, tract_id=None, state=None):
-    response_rates = {}
-
-    responses = get_census_responses(tract_id, state);
-
-    year = get_year(date)
-
-    for resp in responses:
-        rate = resp.rates[year][date]
-        response_rates[resp.tract_id] = rate
-
-    return [{"tract_id": tid, "rate": rate} for tid, rate in response_rates.items()]
-
-'''
 Returns response rate by year
 Parameters:
-    date: string with format YY
+    date: string with format YYYY
     optional tract_id: string 11 digit tract id
     optional state id: string 2 digit state id
 Output:
@@ -59,10 +38,10 @@ Output:
     ]
 The returned rate is the response rate on the last collection day in the year
 '''
-def get_response_rates_by_year(year, tract_id=None, state=None):
+def get_last_response_rates_by_year(year, tract_id=None, state=None):
     response_rates = {}
 
-    responses = get_census_responses(tract_id, state);
+    responses = get_census_responses(tract_id, state)
 
     for resp in responses:
         rates = resp.rates[year]
@@ -70,3 +49,27 @@ def get_response_rates_by_year(year, tract_id=None, state=None):
         response_rates[resp.tract_id] = end_rate
 
     return [{"tract_id": tid, "rate": rate} for tid, rate in response_rates.items()]
+
+
+'''
+Returns all response rates by year
+Parameters:
+    year: string with format YYYY
+    optional tract_id: string 11 digit tract id
+    optional state id: string 2 digit state id
+Output:
+    [
+    {"tract_id": string, "rates": {"date": rate, ...}},
+    ...
+    ]
+'''
+def get_response_rates_by_year(year, tract_id=None, state=None):
+    response_rates = {}
+
+    responses = get_census_responses(tract_id, state)
+
+    for resp in responses:
+        rates = resp.rates[year]
+        response_rates[resp.tract_id] = rates
+
+    return [{"tract_id": tid, "rates": response_rates} for tid, rate in response_rates.items()]
