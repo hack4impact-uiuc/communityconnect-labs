80379ba08f9a617741cdbf62168fa0c0aaa536ae
Danielle Yang <dy6@illinois.edu>
2019-11-17 19:34:08 -0600

    Delete hello world

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -8,15 +8,6 @@ from .response_rates import *
 
 main = Blueprint("main", __name__)  # initialize blueprint
 
-# function that is called when you visit /
-@main.route("/")
-def index():
-    # you are now in the current application context with the main.route decorator
-    # access the logger with the logger from api.core and uses the standard logging module
-    # try using ipdb here :) you can inject yourself
-    logger.info("Hello World!")
-    return "Hello World!"
-
 """
 function that is called when you visit /rate
 Parameters
