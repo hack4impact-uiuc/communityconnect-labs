2264cb33f0c99b3c4f8deb22322f6fffe509aab2
Yousef Ahmed <yousefahmed0066@gmail.com>
2019-11-20 00:22:55 -0600

    Fix rounding of y-axis ticks

--- frontend/src/components/graph.js
+++ frontend/src/components/graph.js
@@ -28,18 +28,26 @@ class Graph extends React.Component {
         for (var key in rates_dict) {
             rates_list.push({"x": rates_dict[key][1], "y": rates_dict[key][0]});
         }
-        console.log(rates_list);
+
         const STEPS = 5
-        const iterator = Math.ceil((rates_list[rates_list.length - 1]["x"] - rates_list[0]["x"]) / STEPS);
+        let iterator = Math.ceil((rates_list[rates_list.length - 1]["x"] - rates_list[0]["x"]) / STEPS);
         let x_label_list = [rates_list[0]["x"]];
 
         for (let i = 1; i <= STEPS; i++) {
           x_label_list.push(iterator + x_label_list[i - 1]);
         }
 
+        iterator = (rates_list[rates_list.length - 1]["y"] - rates_list[0]["y"]) / STEPS;
+        let y_label_list = [Math.round(rates_list[0]["y"] * 10) / 10];
+
+        for (let i = 1; i < STEPS; i++) {
+          y_label_list.push(Math.round((iterator + y_label_list[i - 1]) * 10) / 10);
+        }
+
         this.setState({
             data: rates_list,
-            x_labels: x_label_list
+            x_labels: x_label_list,
+            y_labels: y_label_list
         });
     }
 
@@ -54,7 +62,7 @@ class Graph extends React.Component {
                 />
                 <VictoryAxis
                     dependentAxis
-                    tickValues={[0, .2, .4, .6, .8, 1]}
+                    tickValues={this.state.y_labels}
                     label="Response Rate"
                 />
 
