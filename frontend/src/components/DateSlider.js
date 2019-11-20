2272f4370d52d2d28e4d5293991d45e20fb6cc06
Brandon Wang <sourbrandon@gmail.com>
2019-11-19 22:37:17 -0600

    rename date slider

---
+++ frontend/src/components/DateSlider.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+// const wrapperStyle = { width: 200, margin: 0 };
+
+class DateSlider extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  generateMarks() {
+    var nDates = this.props.dates.length;
+    if (nDates == 1) {
+      return {100: this.props.dates[0]};
+    }
+    var step = 100 / (nDates - 1);
+    var marks = {};
+    var i = 0;
+    this.props.dates.forEach(date => {
+      marks[i] = date;
+      i += step;
+    });
+    return marks;
+  }
+
+  render() {
+    const marks = this.generateMarks();
+    return (
+      <div>
+        <div>
+          <Slider
+            min={0}
+            defaultValue={100}
+            marks={marks}
+            step={null}
+            onChange={ d => this.props.dateChange(marks[d]) } />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DateSlider;
